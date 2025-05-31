0f99d4bf849a7b936d026cded08c6b20f370c672
Facundo <acevedofacundoemir@gmail.com>
2025-05-30 22:16:30 -0300

    Entrega de los puntos 3, 4 y el menú del Parcial I

--- funciones.py
+++ funciones.py
@@ -71,3 +71,102 @@ def porcentaje_aprobados(matriz_alumnos_notas: list):
         resumen_por_alumno.append([matriz_porcentajes_aprobados[i], porcentaje_desaprobado_por_alumno])
     
     return resumen_por_alumno
+
+# PUNTO 3 
+
+def calcular_promedio(lista: list):
+    """
+        Esta función recibe una lista con números y retorna una variable con el promedio
+    """
+    # Creo 2 variables locales, una para acumular todos los valores que contenga la lista y otro para contar cuantos valores hay dentro de esa lista
+    contador_promedio = 0
+    acumulador_promedio = 0
+    # Creo una variable para almacenar el valor del promedio
+    promedio = 0
+    # Recorro la lista pasada por parametro 
+    for i in lista:
+        # Acumulo el/los valores que se encuentren en la lista
+        acumulador_promedio += i
+        # Voy sumando al contador 1 por cada valor que haya dentro de esa lista
+        contador_promedio += 1
+    # Realizo el calculo para saber el promedio de los valores que se encuentren en la lista, es decir, la suma total de los valores dividido la cantidad total de valores que haya
+    promedio = acumulador_promedio / contador_promedio
+    # Retorno la variable con promedio de los valores de la lista
+    return promedio
+
+def lista_de_promedios(matriz_numeros: list):
+    """
+        Esta función recibe una matriz bidimensional de numeros, calcula el promedio de cada sublista y retorna una nueva lista con el promedio de esas sublistas 
+    """
+    # Creo una lista para almacenar todos los promedios
+    promedios = []
+    # Recorro la lista bidimensional para obtener cada sublista
+    for i in matriz_numeros:
+        # Por cada subista creo una variable local para almacenar ese promedio en base a la función creada anteriormente, la cual le paso una lista y me calcula el promedio 
+        promedio_por_sublista = calcular_promedio(i)
+        # Cargo por orden de iteración el promedio de cada sublista a la lista de promedios creada
+        promedios.append(promedio_por_sublista)
+    # Retorno la nueva lista con los promedios de cada sublista
+    return promedios
+
+
+def mejor_promedio(lista_promedios: list):
+    """
+        Esta función recibe una lista con promedios, encuentra el mejor promedio y retorna una variable con el indice del mayor promedio y el mayor promedio 
+    """
+    # Creo 2 variables, una para almacenar el valor del mejor promedio o el mayor y en la segunda para almacenar el indice de ese mejor promedio 
+    mejor_promedio = 0
+    indice_mejor_promedio = 0
+    # Recorro la lista de promedios para obtener su indice
+    for i in range(len(lista_promedios)):
+        # Consulto por cada valor si es mayor al mejor promedio y en caso de serlo, lo almaceno en la variable mejor_promedio
+        if lista_promedios[i] > mejor_promedio:
+            mejor_promedio = lista_promedios[i]
+            # Cargo el indice nuevo mejor promedio
+            indice_mejor_promedio = i + 1
+
+    # Retorno primero el indice y luego el mejor promedio, (esto lo probe y funciona, pero se hacerlo por react, por ejemplo con useState que retorna 1 variable y su funcion setter o cuando creo un custom hooks)
+    # Para acceder a su valor o sus valores se puede hacer de la siguiente forma [indice, promedio] = mejor_promedio(lista) y de esta forma acceder
+    return indice_mejor_promedio, mejor_promedio 
+    
+# PUNTO 4
+# Creo una función para buscar un numero y retornar una lista con los indices donde aparece dicho numero dentro de la lista pasada por parametro
+def buscar_numero(lista:list, numero_buscar: int):
+    """
+        Esta función recibe una lista con números y un número entero que se desea buscar en la lista, busca en que posición se encuentra ese número y retorna una nueva lista con el indice donde se encuentra ese numero, en base a la lista pasada por parametro. 
+    """
+    # Creo una lista para almacenar los indices encontrados
+    lista_numero_buscado = []
+    # Recorro la lista con range y len para obtener el indice 
+    for i in range(len(lista)):
+        # Consulto si el valor es igual al numero que estoy buscando
+        if lista[i] == numero_buscar:
+            # Almaceno ese indice en caso de que el valor de lista sea igual al numero buscado y luego guardo ese valor en la lista creada anteriormente
+            lista_numero_buscado.append( i + 1 )
+    # Retorno la nueva lista con todos los indices donde aparece ese numero buscado
+    return lista_numero_buscado
+
+def buscar_nota(matriz_alumnos_notas: list, nota_buscar: int):
+    """
+        Esta función recibe una matriz de n alumnos y m notas, también recibe un número entero(la nota a buscar) y retorna una nueva matriz con:
+        Indice de alumno con esa nota y una lista con el indice donde aparece esa nota. 
+        Ejemplo: [1, [2, 10]] -> Alumno 1, tiene 1 sola nota de 10 en el indice 2, que representa a la 3er nota de la lista
+    """
+    # Creo una matriz para almacenar el indice del alumno que contenga esa nota a buscar y la cantidad de veces que se repita esa nota con su respectivo indice
+    matriz_nota_buscada = []
+    # Creo una variable local para almacenar un mensaje y retornarlo en ncaso de que no encuentre esa nota
+    mensaje = "No hay ninguna nota con ese valor!"
+    # Recorro la matriz con en para obtener los indices de los alumnos
+    for i in range(len(matriz_alumnos_notas)):
+        # Busco esa nota y la almaceno en una nueva lista, utilizando la funcion previamente creada
+        indice_notas_por_alumno = buscar_numero(matriz_alumnos_notas[i], nota_buscar)
+        # Consulto si la matriz tiene algun valor
+        if len(indice_notas_por_alumno) > 0 :
+            # En caso de tenerlo, guardo los valorers en la matriz creada previamente, es decir, el indice del alumno y la cantidad de veces que se repita esa nota buscada
+            matriz_nota_buscada.append([i + 1, indice_notas_por_alumno])
+    # Consulto si en todos los alumnos encontro esa nota y en caso de no hacerlo, retorne el mensaje previamente creado
+    if len(matriz_nota_buscada) < 1:
+        return mensaje 
+    # En caso de que haya algun alumno con esa nota buscada, lo retorno en la nueva matriz
+    else:
+        return matriz_nota_buscada
--- main.py
+++ main.py
@@ -1,17 +1,116 @@
 # Importo todas las funciones del archivo de funciones
 from funciones import *
 
-# Creo la matriz de notas y las cargo 
-notas = cargar_matriz_notas()
 
-# Muestro todas las notas por cada alumno
-for i in range(len(notas)):
-    for j in range(len(notas[i])):
-        print(f"Las nota {j + 1 } del alumno {i + 1} es: {notas[i][j]}")
+# Creo un menu con las opciones
+lista_opciones = [
+    "------------- MENU DE OPCIONES -------------",
+    "1 - Cargar las notas de los alumnos.",
+    "2 - Obtener el porcentaje de las notas aprobadas y desaprobadas.",
+    "3 - Buscar el mejor promedio de todos los alumnos cargados.",
+    "4 - Buscar una nota especifica",
+    "0 - Salir del menú."
+]
 
-# Llamo a la función que muestre el porcentaje de los aprobados en base a las notas cargadas previamente y la guardo en una nueva matriz
-porcentaje_notas = porcentaje_aprobados(notas)
+# Creo una bandera para que siempre se muestre el menú con sus respectivas opciones
+condicion_menu = True
+# Creo una bandera para que primero carguen las notas de los alumnos
+notas_cargadas = False
+# Creo una lista para almacenar las notas de los alumnos y que siempre se mantengan las mismas para todas las funciones
+notas = []
 
-# Nuestro el resumen de cada alumno, porcentaje de notas aprobadas y desaprobadas
-for i in range(len(porcentaje_notas)):
-    print(f"El alumno {i + 1} aprobó {porcentaje_notas[i][0]} y desaprobo {porcentaje_notas[i][1]}")
+while condicion_menu:
+    # Muestro el menu con las opciones
+    for i in lista_opciones:
+        print(i)
+
+    ingreso_opcion = input("Por favor, ingrese una opción: \n")
+    # Valido si el usuario ingresó un flotante o no es un digito, vuelva a solicitar la opción 
+    while "." in ingreso_opcion or not ingreso_opcion.isdigit():
+        ingreso_opcion = input("Error, por favor, ingrese una opción válida: ")
+    
+    # Acá valido que el usuario haya ingresado una opción entre las 5 que se imprimen
+    while int(ingreso_opcion) < 0 or int(ingreso_opcion) > 4:
+        ingreso_opcion = input("Error, por favor, ingrese una opción válida: ")
+    # Por ultimo parseo la opción ingresada para el match/case
+    ingreso_opcion = int(ingreso_opcion)
+
+    match ingreso_opcion:
+        case 1:
+            # Reescribo la lista global para almacenar las notas 
+            notas = cargar_matriz_notas()
+            print("---------------------------------")
+            # Recorro todaa la lista y muestro las notas de cada alumno
+            for i in range(len(notas)):
+                for j in range(len(notas[i])):
+                    print(f"Alumno {i + 1} nota {j + 1} : {notas[i][j]} ")
+            # Posterior a cargar las notas, cambio el estado de la bandera
+            if not notas_cargadas: 
+                notas_cargadas = True
+        case 2:
+            # Consulto la bandera si ya cargaron las notas
+            if notas_cargadas:
+                # Creo una lista para almacenar los porcentajes aprobados
+                porcentajes = porcentaje_aprobados(notas)
+                # Recorro la nueva lista con range y len para obtener los indices
+                for i in range(len(porcentajes)):
+                    # Muestro los porcentajes de materias aprobadas y desaprobadas de cada alumno, utilizo "i + 1" para mostrar cada alumno de la matriz
+                    print(f"El alumno: {i+1} aprobo { porcentajes[i][0]} % y desaprobo {porcentajes[i][1]} %")
+            # En caso de no estar cargadas las notas, muestro el mensaje para que lo hagan
+            else: 
+                print("Primero debe cargar las notas, seleccione la opción 1")
+        case 3:
+            # Consulto la bandera si ya cargaron las notas
+            if notas_cargadas:
+                # Creo una lista para almacenar los promedios de todos los alumnos
+                promedios = lista_de_promedios(notas)
+                print("---------------------------------")
+                # Creo las 2 variables, indice y promedio, retornadas por la función "mejor_promedio"
+                [indice, promedio] = mejor_promedio(promedios)
+                # Muestro el alumno con el mejor promedio utilizando las 2 variables creadas anteriormente
+                print(f"El mejor promedio es el alumno {indice} con un promedio de {promedio}")
+            # En caso de no estar cargadas las notas, muestro el mensaje para que lo hagan
+            else: 
+                print("Primero debe cargar las notas, seleccione la opción 1")
+        case 4:
+            # Consulto la bandera si ya cargaron las notas
+            if notas_cargadas: 
+                # Le consulto al usuario queu nota quiere buscar
+                nota_a_buscar = input("Ingrese la nota que desea buscar: ")
+                # Valido que sea digito y que no sea un flotante
+                while "." in nota_a_buscar or not nota_a_buscar.isdigit():
+                    nota_a_buscar = print("Error, ingrese la nota que desea buscar de manera correcta (1 a 10): ")
+                # Valido que sea un numero entero
+                while int(nota_a_buscar) < 1 or int(nota_a_buscar) > 10:
+                    nota_a_buscar = print("Error, ingrese la nota que desea buscar de manera correcta (1 a 10): ")
+                # Parseo el número para pasarselo a la función correctamente
+                nota_a_buscar = int(nota_a_buscar)
+                # Creo una matriz con los alumnos que tengan esa nota a buscar
+                notas_buscadas = buscar_nota(notas, nota_a_buscar)
+                # Consulto si la función devuelve una cadena de caracteres, ya que la nota puede no estar entre los alumnos y la muestro
+                if type(notas_buscadas) == str:
+                    print(notas_buscadas)
+                else:
+                    # Recorro la matriz de notas buscadas con len para recorrer el resto de listas
+                    for i in range(len(notas_buscadas)):
+                        # Recorro cada alumno que tenga esa nota
+                        for j in range(len(notas_buscadas[i])):
+                            # Si el tipo de dato es entero, significa que es el número de un alumno que contenga esa nota buscada
+                            if type(notas_buscadas[i][j]) == int:
+                                # Imprimo el alumno que contenga esa nota
+                                print(f"--------- El alumno {notas_buscadas[i][j]} ---------")
+                            else:
+                                # En caso de que el dato no sea tipo string, va a ser una lista por lo cual la recorro para que muestre el indice o examen que contenga esa nota buscada
+                                for k in range(len(notas_buscadas[i][j])):
+                                    # Imprimo cada indice que contenga dicha nota buscada
+                                    print(f"Tiene el examen {notas_buscadas[i][j][k]} con la nota buscada {nota_a_buscar}")
+                                # En caso de no estar cargadas las notas, muestro el mensaje para que lo hagan
+            else: 
+                print("Primero debe cargar las notas, seleccione la opción 1")
+
+        case 0:
+            salir_menu = input("Está seguro que desea salir del menú? (s/n): ").lower()
+            if( salir_menu != "si" and salir_menu !="s"):
+                continue
+            else: 
+                break
